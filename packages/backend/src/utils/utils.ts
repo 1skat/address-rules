e1b8db2363136fa1862f37354e2153cc0df9d7c0
dannylit <itsdanlit@gmail.com>
2026-07-23 20:45:15 +0300

    update: amountToUi function

--- packages/backend/src/utils/utils.ts
+++ packages/backend/src/utils/utils.ts
@@ -1,3 +1,13 @@
+import { Decimal } from "decimal.js";
+
 export const toUiAmount = (amount: bigint, decimals: number): string => {
-    return (Number(amount) / 10 ** decimals).toString();
+    const decimalAmount = new Decimal(amount)
+        .div(10 ** decimals);
+    const abs = decimalAmount.abs();
+
+    return abs.gte(1_000_000_000) ? decimalAmount.div(1_000_000_000).toFixed(2, Decimal.ROUND_DOWN) + "B"
+        : abs.gte(1_000_000) ? decimalAmount.div(1_000_000).toFixed(2, Decimal.ROUND_DOWN) + "M"
+            : abs.gte(1_000) ? decimalAmount.div(1_000).toFixed(2, Decimal.ROUND_DOWN) + "K"
+                : decimalAmount.toFixed(2, Decimal.ROUND_DOWN);
 }
+
